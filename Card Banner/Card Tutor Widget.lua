6c1ee09755b7cacb9a87759a56cae4b503815868
James <32420525+Jtrinh3@users.noreply.github.com>
2024-05-19 19:36:53 -0500

    Create Card Tutor Widget.lua

---
+++ Card Banner/Card Tutor Widget.lua
@@ -0,0 +1,95 @@
+--Idk who made this originally but im using bits and pieces from it.
+function onLoad(saved_data)
+	searchTerm = ''
+	deckDir=-1
+	if saved_data ~= "" then
+		local loaded_data = JSON.decode(saved_data)
+		searchTerm = loaded_data[1]
+		deckDir = loaded_data[2]
+	end
+	if deckDir==1 then
+		lab='→'
+		tip='card extraction: [b]right[/b]'
+	else
+		lab='←'
+		tip='card extraction: [b]left[/b]'
+	end
+	self.createButton({
+		label=lab,
+		tooltip=tip,
+		click_function="changeDeckDir",
+		function_owner=self,
+		position={-1.6,0.1,-1.3},
+		height=200,
+		width=400,
+		font_size=500,
+		font_color={1,1,1,90},
+		color={0,0,0,0},
+	})
+	self.createInput({
+		input_function='getSearch',
+		function_owner=self,
+		label='Banned!',
+		value=searchTerm,
+		position={0,0.1,0},
+		width=1900,
+		height=321,
+		font_size=300,
+		font_color={1,.3,.3,1/.75},
+		color={0,0,0,0.75},
+		tooltip='Commence the banninging',
+		alignment=3
+	})
+end
+function getSearch(obj,ply,val)
+	searchTerm=val
+	updateSave()
+end
+function changeDeckDir()
+	deckDir=deckDir*-1
+	if deckDir==1 then
+		lab='→'
+		tip='card extraction: [b]right[/b]'
+	else
+		lab='←'
+		tip='card extraction: [b]left[/b]'
+	end
+	self.editButton({index=0,label=lab,tooltip=tip})
+  updateSave()
+end
+function updateSave()
+  local data_to_save = {searchTerm,deckDir}
+  saved_data = JSON.encode(data_to_save)
+  self.script_state = saved_data
+end
+
+function onCollisionEnter(co)
+	nowt=os.time()
+	if prevt==nil then prevt=0 end
+	if nowt-prevt<1 then return end
+	prevt=nowt
+	deck = co.collision_object
+	if deck.type == "Deck" then
+    nTake=1
+    nTaken=0
+    for i,card in ipairs(deck.getObjects()) do
+      cname=card.name:lower():gsub('%p','')
+      if cname:match(searchTerm) then
+        nTaken=nTaken+1
+        rot=deck.getRotation()
+        pos=deck.getPosition()
+        rig=deck.getTransformRight()
+        rot[3]=0
+        pos=pos+rig:scale(deckDir*2.4+deckDir*(nTaken-1)*1.5)
+        pos[2]=pos[2]+nTaken*0.1
+        deck.takeObject({index=i-nTaken,position=pos,rotation=rot})
+        if nTaken==tonumber(nTake) then
+          break
+        end
+      end
+    end
+    Wait.time(function() deck.shuffle() end, 0.1, 5)
+
+    self.destruct()
+	end
+end
